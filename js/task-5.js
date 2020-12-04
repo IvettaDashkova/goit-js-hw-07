f9ee2461e4cbe33994f0328defe55ae1db9f33e0
IvettaDashkova <ivettazakonno@gmail.com>
2020-12-05 01:42:25 +0200

    дополнительная задача

--- js/task-5.js
+++ js/task-5.js
@@ -3,7 +3,7 @@ const newName = document.querySelector('input#name-input');
 const oldName = document.querySelector('span#name-output');
 
 const showName = () => {
-    const res = newName.value ?  oldName.textContent = newName.value : oldName.textContent = 'незнакомец';
+  const res = newName.value ?  oldName.textContent = newName.value : oldName.textContent = 'незнакомец';
 }
 
 newName.addEventListener('input', showName);
---
+++ js/task-8.js
@@ -0,0 +1,49 @@
+const btnCreateDivs = document.querySelector('button[data-action="render"')
+
+const btnDeleteDivs = document.querySelector('button[data-action="destroy"')
+
+const input = document.querySelector("div#controls > input")
+
+const box = document.querySelector("div#boxes")
+
+let amount = 0
+let width = 20
+let height = 20
+let color
+
+// колл-бэки
+
+function createBoxes(amount) {
+    amount = +input.value;
+    for (let i = 0; i < amount; i += 1) {
+        width += 10
+        height += 10
+
+		const tagDiv = document.createElement("div")
+		tagDiv.style.backgroundColor = `#${backgroundColorRandom()}`
+		tagDiv.style.width = `${width}px`
+		tagDiv.style.height = `${height}px`
+		box.append(tagDiv)
+	}
+	return
+}
+function destroyBoxes() {
+	box.textContent = ""
+	input.value = 0
+	width = 20
+	height = 20
+	return
+}
+function backgroundColorRandom() {
+	color = Math.round(255.0 * Math.random())
+	let r = color.toString(16)
+	color = Math.round(255.0 * Math.random())
+	let g = color.toString(16)
+	color = Math.round(255.0 * Math.random())
+	let b = color.toString(16)
+	color = r + g + b
+	return color
+}
+// слушатели
+btnCreateDivs.addEventListener("click", () => createBoxes())
+btnDeleteDivs.addEventListener("click", destroyBoxes)
